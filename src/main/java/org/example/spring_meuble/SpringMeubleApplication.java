30c402fefec6d2a1d76f45f90efaf81ff6785704
Margaux Doisy <margauxdoisy@gmail.com>
2024-07-24 15:15:07 +0200

    ajout base de donnée

---
+++ src/main/java/org/example/spring_meuble/SpringMeubleApplication.java
@@ -0,0 +1,13 @@
+package org.example.spring_meuble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMeubleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMeubleApplication.class, args);
+    }
+
+}
