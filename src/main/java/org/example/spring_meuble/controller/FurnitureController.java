41e644017fbcca65130d7652f32f330f90c31c12
Margaux Doisy <margauxdoisy@gmail.com>
2024-07-24 16:56:39 +0200

    ajout html formulaire fourniture

--- src/main/java/org/example/spring_meuble/controller/FurnitureController.java
+++ src/main/java/org/example/spring_meuble/controller/FurnitureController.java
@@ -1,9 +1,12 @@
 package org.example.spring_meuble.controller;
 
+import org.example.spring_meuble.entity.Furniture;
 import org.example.spring_meuble.service.FurnitureService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FurnitureController {
@@ -18,5 +21,16 @@ public class FurnitureController {
     public String index(Model model) {
         model.addAttribute("furnitures", furnitureService.getAllFurnitures());
         return "list";
+    }
+    @GetMapping("/addfurniture")
+    public String addFurniture(Model model) {
+        model.addAttribute("furniture", new Furniture());
+        return "addFurniture";
+    }
+    @PostMapping("/addfurniture")
+    public String addFurniture(@ModelAttribute ("furniture")Furniture furniture) {
+        furnitureService.createFurniture(furniture);
+        return "redirect:/";
+
     }
 }
